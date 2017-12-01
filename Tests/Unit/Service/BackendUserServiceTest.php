7f48b42207711cf09a0a1ceca6907b02b66a316e
Philipp Heckelt <phe@move-elevator.de>
2017-12-01 08:55:05 +0100

    add test for backend user service

---
+++ Tests/Unit/Service/BackendUserServiceTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace MoveElevator\MeBackendSecurity\Tests\Unit\Domain\Model;
+
+use MoveElevator\MeBackendSecurity\Service\BackendUserService;
+use MoveElevator\MeBackendSecurity\Tests\Fixtures\Domain\Model\ExtensionConfigurationFixture;
+use MoveElevator\MeBackendSecurity\Validation\Validator\CapitalCharactersValidator;
+use MoveElevator\MeBackendSecurity\Validation\Validator\CompositeValidator;
+use PHPUnit\Framework\TestCase;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Database\DatabaseConnection;
+use TYPO3\CMS\Saltedpasswords\Salt\SaltFactory;
+
+/**
+ * @package MoveElevator\MeBackendSecurity\Tests\Unit\Domain\Model
+ */
+class BackendUserServiceTest extends TestCase
+{
+    use ExtensionConfigurationFixture;
+
+    protected $backendUserService;
+
+    public function setup()
+    {
+        $backendUserAuthentication = new BackendUserAuthentication();
+        $backendUserAuthentication->user = ['tx_mebackendsecurity_lastpasswordchange' => time()];
+
+        $databaseConnection = new DatabaseConnection();
+
+        $extensionConfiguration = $this->getExtensionConfigurationFixture();
+
+        $capitalCharactersValidator =
+            $this->getMockBuilder(CapitalCharactersValidator::class)
+                ->setMethods(['translateErrorMessage'])
+                ->setConstructorArgs([['extensionConfiguration' => $extensionConfiguration]])
+                ->getMock();
+
+        $capitalCharactersValidator
+            ->method('translateErrorMessage')
+            ->willReturn('translated message');
+
+        $compositeValidator =
+            $this->getMockBuilder(CompositeValidator::class)
+                ->setMethods(['translateErrorMessage'])
+                ->setConstructorArgs([['extensionConfiguration' => $extensionConfiguration]])
+                ->getMock();
+
+        $compositeValidator
+            ->method('translateErrorMessage')
+            ->willReturn('translated message');
+
+        $compositeValidator->append(
+            $capitalCharactersValidator
+        );
+
+        $saltingInstance = SaltFactory::getSaltingInstance(null, 'BE');
+
+        $this->backendUserService = new BackendUserService(
+            $backendUserAuthentication,
+            $databaseConnection,
+            $extensionConfiguration,
+            $compositeValidator,
+            $saltingInstance
+        );
+    }
+
+    public function testCheckPasswordLifeTime()
+    {
+        $result = $this->backendUserService->checkPasswordLifeTime();
+
+        $this->assertNull($result);
+    }
+}
