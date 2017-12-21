7f38f4719997f79e1a266a9dc570c7f49ed03ba0
Philipp Heckelt <phe@move-elevator.de>
2017-12-21 16:12:47 +0100

    add check for same passwords

---
+++ Classes/Validation/Validator/SamePasswordValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MoveElevator\MeBackendSecurity\Validation\Validator;
+
+use MoveElevator\MeBackendSecurity\Domain\Model\PasswordChangeRequest;
+
+/**
+ * @package MoveElevator\MeBackendSecurity\Validation\Validator
+ */
+class SamePasswordValidator extends AbstractValidator
+{
+    const ERROR_CODE = 1513850698;
+
+    /**
+     * @param PasswordChangeRequest $passwordChangeRequest
+     *
+     * @return void
+     */
+    protected function isValid($passwordChangeRequest)
+    {
+        if (empty($passwordChangeRequest->getCurrentPassword())) {
+            return;
+        }
+
+        if ($passwordChangeRequest->getCurrentPassword() !== $passwordChangeRequest->getPassword()) {
+            return;
+        }
+
+        $this->addTranslatedError(self::ERROR_CODE);
+    }
+}
