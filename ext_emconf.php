1bfe618056a06c61faa1a7852d71a54e9fba6928
Philipp Heckelt <phe@move-elevator.de>
2018-02-01 14:31:59 +0100

    update version and readme

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'm:e Backend Security',
     'description' => 'Erweiterte Sicherheit für das TYPO3-Backend',
-    'version' => '1.1.2',
+    'version' => '1.1.3',
     'category' => 'services',
     'constraints' => [
         'depends' => [
