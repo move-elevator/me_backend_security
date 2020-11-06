ad2e779f5b5d725668d28fb5959dea4e9d1a5464
Ronny Hauptvogel <rh@move-elevator.de>
2020-11-06 16:47:13 +0100

    MEINTRA-5012 Update backend user password field configuration

--- Configuration/TCA/Overrides/be_users.php
+++ Configuration/TCA/Overrides/be_users.php
@@ -1,21 +1,34 @@
 <?php
 
+defined('TYPO3_MODE') || die();
+
+use MoveElevator\MeBackendSecurity\Evaluation\PasswordEvaluator;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-$column = [
-    'tx_mebackendsecurity_lastpasswordchange' => [
-        'exclude' => 0,
-        'label' =>
-            'LLL:EXT:me_backend_security/Resources/Private/Language/locallang.xml:' .
-            'be_users.tx_mebackendsecurity_lastpasswordchange',
-        'config' => [
-            'type' => 'input',
-            'renderType' => 'inputDateTime',
-            'eval' => 'datetime',
-            'readOnly' => true
-        ]
-    ],
-];
+(static function () {
+    $languageFile = 'LLL:EXT:me_backend_security/Resources/Private/Language/locallang.xml:';
+    $column = [
+        'tx_mebackendsecurity_lastpasswordchange' => [
+            'exclude' => false,
+            'label' => $languageFile . 'be_users.tx_mebackendsecurity_lastpasswordchange',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+                'readOnly' => true,
+            ],
+        ],
+    ];
 
-ExtensionManagementUtility::addTCAcolumns('be_users', $column);
-ExtensionManagementUtility::addToAllTCAtypes('be_users', 'tx_mebackendsecurity_lastpasswordchange', '', 'after:lastlogin');
+    $GLOBALS['TCA']['be_users']['types'][0]['columnsOverrides'] =
+    $GLOBALS['TCA']['be_users']['types'][1]['columnsOverrides'] = [
+        'password' => [
+            'config' => [
+                'eval' => 'required,' . PasswordEvaluator::class . ',saltedPassword,password',
+            ],
+        ],
+    ];
+
+    ExtensionManagementUtility::addTCAcolumns('be_users', $column);
+    ExtensionManagementUtility::addToAllTCAtypes('be_users', 'tx_mebackendsecurity_lastpasswordchange', '', 'after:lastlogin');
+})();
--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
-$TCA['be_users']['columns']['password']['config']['eval'] =
-    'required,' . \MoveElevator\MeBackendSecurity\Evaluation\PasswordEvaluator::class . ',saltedPassword,password';
