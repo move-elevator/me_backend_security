72460b8c90d9ae87c3e4078f32858749046e3dd6
Ronny Hauptvogel <rh@move-elevator.de>
2020-11-06 16:48:31 +0100

    MEINTRA-5012 Update global variable access

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,17 +1,17 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') || die();
 
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] =
-    MoveElevator\MeBackendSecurity\Hook\UserAuthHook::class . '->postUserLookUp';
+(static function () {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1433416747]['provider'] =
+        \MoveElevator\MeBackendSecurity\LoginProvider\UsernamePasswordLoginProvider::class;
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1433416747]['provider'] =
-    \MoveElevator\MeBackendSecurity\LoginProvider\UsernamePasswordLoginProvider::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] =
+        MoveElevator\MeBackendSecurity\Hook\UserAuthHook::class . '->postUserLookUp';
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][\MoveElevator\MeBackendSecurity\Evaluation\PasswordEvaluator::class] =
-    'EXT:me_backend_security/Classes/Evaluation/PasswordEvaluator.php';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][\MoveElevator\MeBackendSecurity\Evaluation\PasswordEvaluator::class] =
+        'EXT:me_backend_security/Classes/Evaluation/PasswordEvaluator.php';
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['me_backend_security'] =
-    \MoveElevator\MeBackendSecurity\Hook\TableHook::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['me_backend_security'] =
+        \MoveElevator\MeBackendSecurity\Hook\TableHook::class;
+})();
