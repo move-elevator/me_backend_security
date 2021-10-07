bc80d7c50bbf0f65e35daf258407d76ea99a6159
Konstantin Berendakov <konstantin.berendakov@vancado.de>
2021-10-07 18:10:57 +0200

    [FEATURE] Validate password correctly on reset

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,5 @@
 <?php
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 defined('TYPO3_MODE') || die();
 
@@ -14,4 +15,11 @@ defined('TYPO3_MODE') || die();
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['me_backend_security'] =
         \MoveElevator\MeBackendSecurity\Hook\TableHook::class;
+
+
+    if (VersionNumberUtility::convertVersionNumberToInteger(VersionNumberUtility::getNumericTypo3Version()) > 10000000) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Backend\Authentication\PasswordReset::class] = [
+            'className' => MoveElevator\MeBackendSecurity\Authentication\PasswordReset::class
+        ];
+    }
 })();
