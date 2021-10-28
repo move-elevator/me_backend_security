c16330c1a3019b03b47240a7416e23780c8b60c7
Ronny Hauptvogel <rh@move-elevator.de>
2021-10-28 14:55:54 +0200

    MEINTRA-5977 Add password reset xclass to involve me_backend_security password rules during password reset

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,15 +3,25 @@
 defined('TYPO3_MODE') || die();
 
 (static function () {
+    $typo3VersionNumber = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(
+        \TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version()
+    );
+
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1433416747]['provider'] =
         \MoveElevator\MeBackendSecurity\LoginProvider\UsernamePasswordLoginProvider::class;
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] =
-        MoveElevator\MeBackendSecurity\Hook\UserAuthHook::class . '->postUserLookUp';
+        \MoveElevator\MeBackendSecurity\Hook\UserAuthHook::class . '->postUserLookUp';
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][\MoveElevator\MeBackendSecurity\Evaluation\PasswordEvaluator::class] =
         'EXT:me_backend_security/Classes/Evaluation/PasswordEvaluator.php';
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['me_backend_security'] =
         \MoveElevator\MeBackendSecurity\Hook\TableHook::class;
+
+    if ($typo3VersionNumber > 10000000) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Authentication\PasswordReset::class] = [
+            'className' => \MoveElevator\MeBackendSecurity\Authentication\PasswordReset::class
+        ];
+    }
 })();
