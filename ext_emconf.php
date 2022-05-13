0563dabbcab6f0d03ac78e0a1537988e59df2b4c
Ronny Hauptvogel <rh@move-elevator.de>
2022-05-13 08:28:38 +0200

    Reorder properties and update constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,21 +3,18 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'm:e Backend Security',
     'description' => 'Erweiterte Sicherheit für das TYPO3-Backend',
-    'version' => '2.0.7',
     'category' => 'services',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '9.5.0-10.4.99',
-        ],
-        'conflicts' => [
-            'rsaauth' => '',
-        ],
-    ],
-    'state' => 'stable',
-    'clearCacheOnLoad' => true,
     'author' => 'move elevator GmbH',
     'author_email' => 'entwicklung@move-elevator.de',
     'author_company' => 'move elevator GmbH',
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
+    'version' => '2.0.7',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
+    ],
     'autoload' => [
         'psr-4' => [
             'MoveElevator\\MeBackendSecurity\\' => 'Classes',
